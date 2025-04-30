030d444cae38647b25e65fdd7a18b9700368dcd4
Daniel Francisco Alvarez Caballero <95438055+DanielFrnAlvarez@users.noreply.github.com>
2025-04-30 09:37:41 -0500

    Update Tema4.vue
    Se agrega tilde a la SÍNTESIS

--- src/views/curso/Tema4.vue
+++ src/views/curso/Tema4.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
   .curso-main-container.pb-3
-    BannerInterno(:titulo="'Unidad 1. FUNDAMENTACIÓN JURÍDICA'" :subTitulo="'SINTESIS'")
+    BannerInterno(:titulo="'Unidad 1. FUNDAMENTACIÓN JURÍDICA'" :subTitulo="'SÍNTESIS'")
     .container.tarjeta.tarjeta--blanca.p-4.p-md-5
       
       p(data-aos="fade-up").mb-5 #[strong La unidad 1:  Introducción al derecho y sus fuentes], establece los fundamentos teóricos esenciales para comprender el origen de las normas jurídicas y su aplicación. Esta unidad sienta las bases de la formación jurídica que se desarrollará a lo largo del curso, proporcionando una perspectiva clara sobre el concepto de Derecho, sus funciones en la sociedad y el origen de las normas.   Este enfoque integral asegura que los estudiantes adquieran una comprensión sólida de los fundamentos del Derecho, lo que les permitirá avanzar con éxito en su estudio y práctica futura.
